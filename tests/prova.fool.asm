e38cd225d9b00e277fb053ca7d06755cec67c28d
Giuseppe De Palma <depalma.gsp@gmail.com>
2018-11-13 12:51:44 +0100

    Working on functions that perfom assignments

--- tests/prova.fool.asm
+++ tests/prova.fool.asm
@@ -1,43 +1,18 @@
 ## LET
 
 push function0
-push function1
 push 12
-push 3
 
 ## IN
 
-push -3
-lfp
-add
-lw
-push 6
-div
-lfp
-push -3
-add
-sw
-push -3
-lfp
-add
-lw
 lfp
 lfp
-push -2
+push -1
 lfp
 add
 lw
 js
-mult
-lfp
-push -4
-add
-sw
-push -3
-lfp
-add
-lw
-push -4
+push -2
 lfp
 add
 lw
@@ -46,19 +21,12 @@ halt
 function0:
 cfp
 lra
-push 115
-srv
-sra
-pop
-sfp
-lrv
-lra
-js
-
-function1:
-cfp
-lra
-push 37
+push 42
+lfp
+lw
+push -2
+add
+sw
 srv
 sra
 pop
